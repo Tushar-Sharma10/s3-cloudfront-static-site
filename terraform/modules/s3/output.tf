e18586a56859e2e67e2360bd846e96c9528d73d5
Tushar Sharma <tusharsharma.techsavvy@gmail.com>
2026-04-25 16:27:41 -0400

    Add output for bucket domain name

--- terraform/modules/s3/output.tf
+++ terraform/modules/s3/output.tf
@@ -7,3 +7,7 @@ output "bucket_arn" {
   description = "ARN of the S3 bucket"
   value       = aws_s3_bucket.origin_bucket.arn
 }
+
+output "bucket_domain_name" {
+  value = aws_s3_bucket.origin_bucket.bucket_domain_name
+}
